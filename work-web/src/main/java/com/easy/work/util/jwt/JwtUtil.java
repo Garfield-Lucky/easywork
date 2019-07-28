c32b37d41e9c9a8fb663cfbdd3500e7bfcddb59a
Garfield-Lucky <wzw971268>
2019-07-28 19:07:53 +0800

    优化jwt

--- work-web/src/main/java/com/easy/work/util/jwt/JwtUtil.java
+++ work-web/src/main/java/com/easy/work/util/jwt/JwtUtil.java
@@ -9,14 +9,20 @@ import com.auth0.jwt.interfaces.Claim;
 import com.easy.work.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+
+@Component
 public class JwtUtil {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
-    public static final long EXPIRATION_TIME = 3600 * 1000; // 失效时间为一小时
-    public static final String SECRET = "wuzhangwei"; //密钥，这个密钥不能泄露给别人，否则被人就能伪造你的token
+
+    public static long EXPIRATION_TIME ; //失效时间
+    public static String TOKEN_SECRET ; //加密密码
 
      /**
       * @Description:生成token
@@ -26,7 +32,7 @@ public class JwtUtil {
     public static String generateTokenWithAudience(User user) {
         String token = JWT.create().withAudience(user.getId().toString()) // 将 user id 保存到 token 里面
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME)) //过期时间
-                .sign(Algorithm.HMAC256(SECRET));//密钥
+                .sign(Algorithm.HMAC256(TOKEN_SECRET));//密钥
         return token;
     }
 
@@ -39,7 +45,7 @@ public class JwtUtil {
         String token = JWT.create()
                 .withClaim("userId", user.getId()) // 将 user id 保存到 token 里面
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME)) //过期时间
-                .sign(Algorithm.HMAC256(SECRET));//密钥
+                .sign(Algorithm.HMAC256(TOKEN_SECRET));//密钥
         return token;
     }
 
@@ -49,7 +55,7 @@ public class JwtUtil {
       * @author Created by wuzhangwei on 2019/7/27
       */
     public static Map<String, Claim> validateTokenAndGetClaims(String token) {
-        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(SECRET)).build();
+        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(TOKEN_SECRET)).build();
         Map<String, Claim> claim = null;
         try {
             claim = jwtVerifier.verify(token).getClaims();
@@ -69,7 +75,7 @@ public class JwtUtil {
      * @author Created by wuzhangwei on 2019/7/27
      */
     public static List<String> validateTokenAndGetAudience(String token) {
-        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(SECRET)).build();
+        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(TOKEN_SECRET)).build();
         List<String> audience = null;
         try {
             audience = jwtVerifier.verify(token).getAudience();
@@ -82,4 +88,33 @@ public class JwtUtil {
         }
         return audience;
     }
+
+     /**
+      * @Description:验证token
+      * @param
+      * @author Created by wuzhangwei on 2019/7/27
+      */
+    public static void validateToken(String token) {
+        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(TOKEN_SECRET)).build();
+        try {
+            jwtVerifier.verify(token);
+        } catch (JWTDecodeException e) {
+            logger.error("validateTokenAndGetAudience: token解析失败 {}", e.getMessage());
+            throw new JWTDecodeException(e.getMessage());
+        } catch (JWTVerificationException e) {
+            logger.error("validateTokenAndGetAudience: token验证失败 {}", e.getMessage());
+            throw new JWTVerificationException(e.getMessage());
+        }
+    }
+
+    @Value("${config.jwt.expire-time}")
+    public void setExpirationTime(long expirationTime) {
+        EXPIRATION_TIME = expirationTime;
+    }
+
+    @Value("${config.jwt.secret}")
+    public void setTokenSecret(String tokenSecret) {
+        TOKEN_SECRET = tokenSecret;
+    }
+
 }
